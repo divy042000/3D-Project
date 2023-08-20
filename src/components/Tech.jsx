9aaca5962f58f7f768661e7b8bc7bada1e9f00fc
Patel Divy <divypatel0403@gmail.com>
2023-08-20 20:25:56 +0530

    Add  Ball

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,24 @@
-// import React from 'react'
-
+import React from 'react'
+// import  Ball  from "./Ball.jsx";
+import { SectionWrapper } from '../hoc'
+import { technologies } from '../constants'
+import BallCanvas from './Ball.jsx';
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+     {technologies.map((technology)=>(
+        <div className="w-28 h-28" key={technology.name}>
+           <BallCanvas icon={technology.icon}/>
+
+
+        </div>
+     ))}
+      
+      
+      
+      
+      </div>  
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech,""); 
